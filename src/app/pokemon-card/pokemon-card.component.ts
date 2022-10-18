9bbcd2ed78b7382f8e093c5485cbad66c7ef7db9
Lourencobrah <lourencospecter@gmail.com>
2022-10-18 09:17:07 -0300

    Initial angular components

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.css']
+})
+export class PokemonCardComponent {
+  @Input()
+  pokemon!: string;
+
+  @Input()
+  idPokemon!: number;
+
+  getImagePokemon(){
+    const formatNumber = this.leadingZero(this.idPokemon);
+
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${formatNumber}.png`;
+  }
+
+  leadingZero(str: string | number, size = 3): string {
+    let s = String(str);
+
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+
+    return s;
+  }
+}
